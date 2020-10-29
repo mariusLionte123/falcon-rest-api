e5f579cb4f9057c916575d63eeeece876d976971
Marius <c_Marius.Lionte@viasat.com>
2020-10-29 16:36:34 +0200

    implement rest api features

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import falcon, json
+from uuid import uuid4
 
 from db import Database
 
@@ -9,84 +10,65 @@ class ObjRequestClass:
     def __validate_json_input(self, req: falcon.Request):
         try:
             self.__json_input = json.loads(req.stream.read())
-            return True
         except ValueError:
             self.__json_input = {}
-            return False
 
-    def on_get(self, req: falcon.request.Request, res: falcon.Response):
-        input_is_valid = self.__validate_json_input(req)
-
+    def on_get(self, req: falcon.request.Request, res: falcon.Response, product_id=None):
         res.status = falcon.HTTP_200
 
-        if input_is_valid and "id" in self.__json_input:
-            result = database.get(self.__json_input["id"])
-            if len(result) == 0:
-                res.status = falcon.HTTP_404
-                res.body = json.dumps({"message":"Product not Found", "error": True})
-                return
-            else:
-                product, = result
-                res.media = product
-                return
-        else:
+        if product_id is None:
             result = database.get()
             res.media = result
-
-    def on_post(self, req:falcon.Request, res: falcon.Response):
-        input_is_valid = self.__validate_json_input(req)
-
-        if input_is_valid == False:
-            res.status = falcon.HTTP_422
-            response = {
-                "message": "invalid input",
-                "error": True
-            }
-            res.media=response
-
-        res.status = falcon.HTTP_201
-        
-        response = database.insert(self.__json_input)
-
-        res.media=response
-
-    def on_put(self, req:falcon.Request, res: falcon.Response):
-        input_is_valid = self.__validate_json_input(req)
-
-        res.status = falcon.HTTP_422
-        if input_is_valid == False:
-            response = {"error": True, "message": "Invalid input format"}
-            res.media = response
             return
 
-        if "id" not in self.__json_input:
+        
+        result = database.get(product_id)
+        if len(result) == 0:
+            res.status = falcon.HTTP_404
+            res.body = json.dumps({"message":"Product not Found", "error": True})
+            return
+        else:
+            product, = result
+            res.media = product
+                
+            
+
+    def on_post(self, req:falcon.Request, res: falcon.Response):
+        self.__validate_json_input(req)
+
+        print(self.__json_input)
+        self.__json_input.__setitem__("id", str(uuid4()))
+        response = database.insert(self.__json_input)
+
+        res.status = falcon.HTTP_201
+        res.media=response
+
+    def on_put(self, req:falcon.Request, res: falcon.Response, product_id=None):
+        if product_id is None:
             response = {"error": True, "message": "You must provice a valid id"}
             res.media = response
             return
+
+        self.__validate_json_input(req)
+
+        self.__json_input["id"] = product_id
         
         updated_user = database.update(self.__json_input)
 
         res.status=falcon.HTTP_200
         res.media = updated_user
 
-    def on_delete(self, req: falcon.Request, res: falcon.Response):
-        input_is_valid = self.__validate_json_input(req)
-
-        res.status = falcon.HTTP_422
-        if input_is_valid == False:
-            response = {"error": True, "message": "Invalid input format"}
-            res.media = response
-            return
-
-        if "id" not in self.__json_input:
+    def on_delete(self, req: falcon.Request, res: falcon.Response, product_id=None):
+        if product_id is None:
             response = {"error": True, "message": "You must provice a valid id"}
             res.media = response
             return
 
-        database.delete(self.__json_input["id"])
+        database.delete(product_id)
 
         res.status = falcon.HTTP_200
         res.media = {"message": "Product deleted"}
 
 api = falcon.API()
-api.add_route('/api', ObjRequestClass())
+api.add_route('/v1/products', ObjRequestClass())
+api.add_route('/v1/products/{product_id}', ObjRequestClass())
